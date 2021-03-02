4d792e785272e9b4ca4d7cfd658184f38ce08d17
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-03-02 13:48:28 -0500

    commit tree 25367a287549727e4e4d684af42b65ac0336f84c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function mapToNegativize(arr) {
+    return arr.map(i => -i)
+}
+
+function mapToNoChange(arr) {
+    return arr.map(i => i)
+}
+
+function mapToDouble(arr) {
+    return arr.map(i => 2 * i)
+}
+
+function mapToSquare(arr) {
+    return arr.map(i => i ** 2)
+}
+
+function reduceToTotal(arr, start = 0) {
+    return arr.reduce((total, i) => total + i, start)
+}
+
+function reduceToAllTrue(arr) {
+    return arr.reduce((and, each) => and && !!each, true)
+}
+
+function reduceToAnyTrue(arr) {
+    return arr.reduce((or, each) => or || !!each, false)
+}
